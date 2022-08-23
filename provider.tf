c4e7fc97f4973f8d91d1410e47af46d31bdf479e
Daniel Marquez <danielarturo.marquez@gmail.com>
2022-08-23 14:49:52 -0400

    first Commit

---
+++ provider.tf
@@ -0,0 +1,7 @@
+# <PROVIDER_NAME> can be = aws,azurerm, google, linode, digitalocean, heroku
+# Documentation : https://registry.terraform.io/browse/providers
+provider "<PROVIDER_NAME>" {}
+
+# Example :
+
+provider "google" {}
